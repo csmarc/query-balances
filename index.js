15bb0eba663e4aea2932cae83d7cde9d8d1b3625
csmarc <marton.csernai@gmail.com>
2021-08-26 13:34:10 +0200

    clean up

--- index.js
+++ index.js
@@ -1,23 +1,9 @@
-import {
-  BlockchainUtils,
-  connect,
-  disconnect,
-  Identity,
-  init,
-} from "@kiltprotocol/sdk-js";
-import BN from "bn.js";
-
-// Convert a big number balance to expected float with correct units.
-function toUnit(balance) {
-  decimals = bc.api.registry.chainDecimals;
-  base = new BN(10).pow(new BN(decimals));
-  dm = new BN(balance).divmod(base);
-  return parseFloat(dm.div.toString() + "." + dm.mod.toString());
-}
+import { BlockchainUtils, connect, disconnect, Identity, init } from '@kiltprotocol/sdk-js';
 
 async function main() {
   await init({
-    address: "wss://westend.kilt.io",
+    // address: 'wss://westend.kilt.io',
+    address: 'wss://spiritnet.kilt.io',
     logLevel: 0,
   });
 
@@ -25,31 +11,20 @@ async function main() {
   console.log(bc.api.genesisHash.toHex());
   const query = await bc.api.query.system.account.entries();
   // const entries = query.map(([key, value]) => [key.args, k.value])
-  console.log(query[0]);
-  console.log(
-    '"address";        "free balance";"locked amount";"lock until";"vest amount";"vest per block"'
-  );
+  // console.log(query[0]);
+  console.log('"address";        "free balance";"locked amount";"lock until";"vest amount";"vest per block"');
 
   let lines = query.map(async ([key, account]) => {
-    let address = key.args.map((k) => k.toHuman())[0];
-    let lock_info = (await bc.api.query.kiltLaunch
-      .balanceLocks(address)).unwrapOrDefault();
-    let vest_info = (await bc.api.query.vesting
-      .vesting(address))
-      .unwrapOrDefault();
+    let address = key.args.map(k => k.toHuman())[0];
+    let lock_info = (await bc.api.query.kiltLaunch.balanceLocks(address)).unwrapOrDefault();
+    let vest_info = (await bc.api.query.vesting.vesting(address)).unwrapOrDefault();
 
-    return `"${
-      key.args.map((k) => k.toHuman())[0]
-    }";${account.data.free.toString()};${lock_info.amount};${lock_info.block};${
-      vest_info.locked
-    };${vest_info.perBlock}`;
+    return `"${key.args.map(k => k.toHuman())[0]}";${account.data.free.toString()};${lock_info.amount};${
+      lock_info.block
+    };${vest_info.locked};${vest_info.perBlock}`;
   });
 
   let lines2 = await Promise.all(lines);
-  lines2.forEach((l1) => console.log(l1));
-
-  // const a = new BN('000000000000000d8d726b7177a80000', 16);
-  // console.log(a.toString(10));
-  // console.log(toUnit(a.toString(10)))
+  lines2.forEach(l1 => console.log(l1));
 }
 main().finally(disconnect);
